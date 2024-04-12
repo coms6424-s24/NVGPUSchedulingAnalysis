82fbf02c8f2602ded2dfa83cf9023b45b80af884
XimingChen <ximingc1219@gmail.com>
2024-04-11 20:54:18 -0400

    Executed exp6

--- scripts/build_exp6.bat
+++ scripts/build_exp6.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set VSCOMPILER="D:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.34.31933/bin/Hostx64/x64"
+set VSCOMPILER="E:\visual studio\VC\Tools\MSVC\14.39.33519\bin\Hostx64\x64"
 set SRCFILES=../src/Experiment6.cu ../src/KernelsExp6.cu
 set OUTPUT=-o Experiment6.exe
 set INCLUDES=-I "../include"
